e60dfab2aabbefdff598aa7a77e294d52ccb54ad
Theosd1 <159036943+Theosd1@users.noreply.github.com>
2024-10-10 20:26:26 +0200

    Add files via upload

---
+++ Explication de ce que je n'arrive pas.R
@@ -0,0 +1,14 @@
+# EXPLICATION DE MES DIFFICULTES
+
+# POur LE SHINNY ET LA Partie graphique, je n'arrive pas a afficher le premier
+#graphique avec le nombre de logement par code postal
+
+#j'ai changer le code de l'api, j'ai 2 fichiers CSV, neuf et ancien et on
+# peut choisir le fichier si on veut les données pour les logements neufs ou ancien
+
+-# j'ai du mal a afficher la carte , je souhaite afficher la carte de lyon,
+# carte des logements de lyon selon le type de logement ( appartement ou immeuble)
+  # et avoir des infos quand on clique sur un points gps (etiquette_dpe, code postal(ban),...)
+# il faudrait pouvoir aussi actualiser les données ( raffraichir)
+  
+# troisieme onglet, si tu peux demander de creer un tableau avec les données utiliser 
